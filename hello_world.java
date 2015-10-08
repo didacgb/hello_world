29b60f8518b4b1a0cc093efdbfcd504d16656769
didacgb <snog@hotmail.es>
2015-10-08 16:33:37 +0200

    seneca

--- hello_world.java
+++ hello_world.java
@@ -3,6 +3,6 @@ package prova1;
 
 public class hello_world {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("Hello World: To forgive all is as inhuman as to forgive none (Seneca). Fortunately we have GIT");
     }
 }
