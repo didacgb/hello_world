f39ba4b28b87bdbed78c0ed86d4cb37b5e50d78d
didacgb <snog@hotmail.es>
2015-10-08 16:42:29 +0200

    from vilanova

--- hello_world.java
+++ hello_world.java
@@ -3,6 +3,6 @@ package prova1;
 
 public class hello_world {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("Hello World from Vilanova");
     }
 }
